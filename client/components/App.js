0debce5998f2e9d57a6be851b704d776d536039b
Shehbaz Khan <baz6066@gmail.com>
2018-05-30 06:41:30 -0500

    Added components under client. Made a simple App component for testing purpose. Updated package.json for webpack to auto-watch.

---
+++ client/components/App.js
@@ -0,0 +1,19 @@
+// const App = () => {
+//     return (
+//         <h2>Hello Components</h2>
+//     );
+// }
+
+class App extends React.Component {
+    constructor() {
+        super();
+    }
+    
+    render() {
+        return (
+            <h2>Hello Components</h2>
+        );
+    }
+}
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,4 +1,6 @@
+import App from './components/App.js';
+
 ReactDOM.render(
-    React.createElement("h2", null, "Hello React"),
+    <App/>,
     document.getElementById("root")
 );
