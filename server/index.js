97a95da5dc8095a4b647f41d7707b9792502dde4
Shehbaz Khan <baz6066@gmail.com>
2018-06-07 17:50:12 -0500

    Added cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 8080;
+
+app.use(cors());
 
 app.use(express.static('client'));
 app.set('view engine', 'ejs');
