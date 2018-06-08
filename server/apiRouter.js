29945003931792dda2aaa3a7459ce0153535d6f8
Shehbaz Khan <baz6066@gmail.com>
2018-06-07 19:12:10 -0500

    Uploaded database to Heroku so it can accessed remotely now

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const pg = require('pg');
 
+//postgres://ihhcrpadlvhspt:b9605f87b178246f588943ee418a3b1432a33831533dee7dbbd1b2f90127acff@ec2-23-23-226-190.compute-1.amazonaws.com:5432/dfe14rc8ahsnn8
+
 const pool = new pg.Pool({
-    user: 'postgres',
-    database: 'postgres',
-    password: '1234',
-    host: 'localhost',
-    port: 5432
+    user: 'ihhcrpadlvhspt',
+    database: 'dfe14rc8ahsnn8',
+    password: 'b9605f87b178246f588943ee418a3b1432a33831533dee7dbbd1b2f90127acff',
+    host: 'ec2-23-23-226-190.compute-1.amazonaws.com',
+    port: 5432,
+    ssl: true
 });
 
 const router  = express.Router();
