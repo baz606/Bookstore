849f66986e75a5f0283d7c2662dd0aecd46451db
Shehbaz Khan <baz6066@gmail.com>
2018-07-15 03:57:19 -0500

    Changed client to src in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, 'client'),
+    path: path.join(__dirname, 'src'),
     filename: 'bundle.js'
   },
   mode: 'development',
