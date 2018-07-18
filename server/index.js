635c574f772668612f9579ec4edbc53891e72b20
Shehbaz Khan <baz6066@gmail.com>
2018-07-18 05:28:22 -0500

    Updated server/index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const port = 8080;
 
 app.use(cors());
 
-app.use(express.static('client'));
+app.use(express.static('src'));
 app.set('view engine', 'ejs');
 
 //import serverRender from './render';
