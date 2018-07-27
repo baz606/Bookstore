eb3b68c9e3b6c9458721021d54e0569e30bdc6c2
Shehbaz Khan <baz6066@gmail.com>
2018-07-26 22:20:08 -0500

    Updated fetchRatingForBook method in App to console log the rating data from the api

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import BookList from './BookList';
+import axios from 'axios';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            books: this.props.initialData
+            books: this.props.initialData,
+            ratings: {}
         };
     }
 
     fetchRatingForBook(bookId) {
-        console.log(`Book ${bookId} clicked!`);
+        axios.get(`http://localhost:8080/api/books/${bookId}/ratings`)
+             .then(resp => {
+                 console.log(resp.data)
+             })
     }
 
     render() {
